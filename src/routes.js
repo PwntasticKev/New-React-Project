70902fe62a62ba3e66fab46542d19909a4fa9088
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-09-06 20:31:54 -0600

    Added PageViews and assets folder

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
-import HomePage from "./Components/HomePage"
+import HomePage from "./Views/HomePage"
 
 export default (
   <Switch>
